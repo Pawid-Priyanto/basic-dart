8d6a61323376854e79ae9206ff1083e57823bbc3
Pawid-prianto27 <pawitanto27@gmail.com>
2020-03-26 20:23:06 +0700

    list

--- hello.dart
+++ hello.dart
@@ -1,12 +1,10 @@
-// void main() {
-//  print(add(35, 15));
-// }
+void main(){
+  List<int> myList = [8,34, 9];
+  print(myList[0]);
 
+  List<String> name = ["Kevin", "Nerd", "Yusuf"];
+  print(name[2]);
 
-// int add(int x, int y){
-//   return x * y;
-// }
-
-void main() => print(add(35, 15));
-
-int add(int x, int y) => x * y;
+  List<dynamic> differentType = ["covid-19", true, 29, 1.9];
+  print(differentType[0]);
+}
