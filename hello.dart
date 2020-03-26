7dda718b1cf17dad42abac52b40f5014e734ab99
Pawid-prianto27 <pawitanto27@gmail.com>
2020-03-26 14:47:59 +0700

    variables-interaction

--- hello.dart
+++ hello.dart
@@ -1,21 +1,21 @@
 void main(){
  int number = 23;
+ int number2 = 76;
 
  String name = "Bakrie";
+ String last_name = "umarella";
 
+ print( number * number2); // we can use all of math function
 
+ print("my name is " + name + " " + last_name);
+ print("my firstname is $name");
+ print("my lastname is $last_name");
 
 /**
- * constant
- * constant variables can't be assign a new value
+ * note
+ * in variables-interaction variables must add $ if you were not add $ that's not call as variables
  */
 
-const username = "kevin hermawan";
 
- print(username);
- print(name);
- print(number);
- number = 90;
- print(number);
 
 }
