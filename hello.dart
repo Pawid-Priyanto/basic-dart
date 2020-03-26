68da8411af914b01d2b116ad040612c5de4633c9
Pawid-prianto27 <pawitanto27@gmail.com>
2020-03-26 14:13:14 +0700

    firstcommit

---
+++ hello.dart
@@ -0,0 +1,17 @@
+void main(){
+ int number = 23;
+
+ String name = "Bakrie";
+
+ double bilbul = 2.2;
+
+ bool isActive = false;
+
+ var male = true;
+
+ print(name);
+ print(number);
+ print(bilbul);
+ print(isActive);
+ print(male);
+}
