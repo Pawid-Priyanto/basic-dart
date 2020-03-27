b514eb3dd54fb78c0ede8e9d64108be5c82e4f0c
Pawid-prianto27 <pawitanto27@gmail.com>
2020-03-27 16:51:51 +0700

    overwritingmethod

--- hello.dart
+++ hello.dart
@@ -2,13 +2,22 @@ void main(){
   var buudy = Pet();
   buudy.age = 8;
   print(buudy.age);
+  buudy.hasBirthday();
 
 }
 
 class Pet extends animal{
  String name;
+
+ void hasBirthday(){
+   super.hasBirthday();
+ }
 }
 
 class animal{
   int age;
+
+  void hasBirthday(){
+    print("Happy Birthday");
+  }
 }
