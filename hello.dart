0bcbefc5d3fd4837f66ed3c1146fd407abc0255e
Pawid-prianto27 <pawitanto27@gmail.com>
2020-04-07 20:39:11 +0700

    function_01

--- hello.dart
+++ hello.dart
@@ -1,8 +1,21 @@
-void main(){
-  Map<String, dynamic> persons = {
-    "Prabu" : 43,
-    "Baduy" : 23,
-    "Andri" : "aku"
-  };
-  print(persons["Andri"]);
+// A functtion declaration
+int timesTwo(int x){
+  return x * 2;
+}
+
+// Arrow syntax is shorthand for `{ return expr }`
+int timesFour(int x) => timesTwo(timesTwo(x));
+
+// Function are object.
+int runTwice(int x, Function f) {
+  for (var i = 0; i < 2; i++) {
+    x = f(x);
+  }
+  return x;
+}
+
+main() {
+  print("4 times 2 is ${timesTwo(4)}");
+  print("4 times 4 is ${timesFour(4)}");
+  print("2 x 2 x 2 is ${runTwice(2, timesTwo)}");
 }
