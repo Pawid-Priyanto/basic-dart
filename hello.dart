8ad5ee7a75e61af9042f00f496f53d37c5eb516c
Pawid-prianto27 <pawitanto27@gmail.com>
2020-04-08 15:53:38 +0700

    string

--- hello.dart
+++ hello.dart
@@ -1,12 +1,18 @@
-void main() {
-  sayHi("Pacar");
-  add(9, 34);
+main() {
+  print('string pakai petik satu');
+  print("String pake petik dua");
+
+  // String can be combined with the + operator.
+  print('Cat' + 'Dog');
+
+  // Triple quotes define a multi-line string
+  print('''triple quoted string
+  are for multiple lines''');
+
+  //  Dart supports string interpolation.
+  const pi = 3.14;
+  print('pi is $pi');
+  print('hasil dari ${2 * pi}');
+
 }
 
-void sayHi(String status){
-  print("hai $status");
-}
-
-void add(int x, int y){
-  print(x * y);
-}
