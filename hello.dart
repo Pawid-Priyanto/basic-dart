de1d5398be07bb3fca7a1823a1dc5b846795d43d
Pawid-prianto27 <pawitanto27@gmail.com>
2020-04-08 18:31:21 +0700

    while

--- hello.dart
+++ hello.dart
@@ -1,24 +1,9 @@
-bool isEven(int x) {
-  // An if-else statement
-  if (x % 5 == 0) {
-    return true;
-  } else {
-    return false;
+main() {
+  int x = 0;
+  while (x < 100){
+    print("antrian ke $x");
+    x++;
   }
 }
 
-List<int> getEvenNumbers(Iterable<int> numbers) {
-  var evenNumbers = <int>[];
-
-  // A for-in loop
-  for (var i in numbers) {
-    // A single-line if statement.
-    if(isEven(i)) evenNumbers.add(i);
-  }
-  return evenNumbers;
-}
-
-main(List<String> args) {
-  var numbers = List.generate(20, (i) => i);
-  print(getEvenNumbers(numbers));
-}
+// same with for bedanya pada pengecekan
