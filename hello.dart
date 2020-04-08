f4ce837f03a5b6923024226c96a5b67b12884be5
Pawid-prianto27 <pawitanto27@gmail.com>
2020-04-08 19:07:30 +0700

    setterandgetter

--- hello.dart
+++ hello.dart
@@ -1,18 +1,26 @@
-main() {
- dynamic nilai = "A";
- switch(nilai){
-   case "C" :
-   print("nilai kamu dibawah kkm");
-   break;
-   case "B" :
-   print("nilai kamu standart banget");
-   break;
-   case "A" : 
-   print("nilai kamu bagus");
-   break;
-   default :{
-     print("nilai kamu tidak ada");
-   }
- }
+main(){
+  var myBike = new Bike("Blue", "United", 25000); // default setter
+  print(myBike.color);
+  print(myBike.merk);
+  print(myBike.price);
+
+
+  myBike.harga = 380.0;
+  print(myBike.harga);
 }
 
+class Bike {
+  String color; // default getter
+  String merk;
+  double price;
+
+  Bike(this.color, this.merk, this.price);
+
+  double keuntungan;
+  set harga (double hargaPasar){
+    keuntungan = (hargaPasar / 100 ) * 25; // custom setter
+  }
+  double get harga{
+    return keuntungan; // custom getter
+  }
+}
