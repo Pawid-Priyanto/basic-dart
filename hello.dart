1faa7b10b73806dd9ca6f129f4e48419a8b3effc
Pawid-prianto27 <pawitanto27@gmail.com>
2020-04-08 16:19:13 +0700

    classes

--- hello.dart
+++ hello.dart
@@ -1,18 +1,39 @@
-main() {
-  print('string pakai petik satu');
-  print("String pake petik dua");
-
-  // String can be combined with the + operator.
-  print('Cat' + 'Dog');
-
-  // Triple quotes define a multi-line string
-  print('''triple quoted string
-  are for multiple lines''');
-
-  //  Dart supports string interpolation.
-  const pi = 3.14;
-  print('pi is $pi');
-  print('hasil dari ${2 * pi}');
-
+// Abstract classes can't be instantiated
+abstract class Item {
+  use();
 }
 
+// Classes can implement other classes.
+class Chest<T> implements Item {
+  List<T> contents;
+
+  // Constructor can assign arguments to instance variables using 'this'.
+  Chest(this.contents);
+
+  use() => print("$this has ${contents.length} items");
+}
+
+class Sword implements Item {
+  int damage = 10;
+
+  use() => print("$this dealt $damage damage");
+}
+
+// Classes can extends other classes
+class DiamondSword extends Sword {
+  int damage = 89;
+}
+
+main() {
+  // The new 'keyword' is optional.
+  var chest = Chest<Item>([
+    DiamondSword(),
+    Sword(),
+  ]);
+
+  chest.use();
+
+  for (var item in chest.contents) {
+    item.use();
+  }
+}
